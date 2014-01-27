93a237aaf007ad9fcfd1fd2685a4a58664dd5f5e
Travis Maynard <hello@travismaynard.com>
2014-01-27 03:00:03 -0500

    Improved gulpfile in preparation for upcoming Hack Day.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
 var myth = require('gulp-myth');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
 
-gulp.task('compile', function() {
-    gulp.src('./app/assets/css/main.css')
-        .pipe(rename('main-prod.css'))
+gulp.task('styles', function() {
+    gulp.src('./app/assets/css/*.css')
+        .pipe(concat('app.css'))
         .pipe(myth())
-        .pipe(gulp.dest('./app/assets/css'));
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src('./app/**/*.js')
+        .pipe(jshint())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 // Default Task
 gulp.task('default', function(){
-    gulp.run('compile');
+    gulp.run('styles', 'scripts');
 
     // Watch For Changes To Our JS
-    gulp.watch('./app/assets/css/main.css', function(){
-        gulp.run('compile');
+    gulp.watch('./app/assets/css/*.css', function(){
+        gulp.run('styles');
+    });
+
+    gulp.watch('./app/**/*.js', function() {
+        gulp.run('scripts');
     });
 });
